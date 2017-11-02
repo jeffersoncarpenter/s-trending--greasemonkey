cfbb527213d6d1ab2329c21b5454d494811771d0
Jefferson Carpenter <jeffersoncarpenter2@gmail.com>
2017-11-02 18:10:13 -0500

    Create userscript.js

---
+++ userscript.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name           Trending Censor
+// @namespace      Censor
+// @description    Profanity censorship on the web.
+// @include        *
+// ==/UserScript==
+
+
+(function() {
+
+   var textnodes, node, s; 
+
+   textnodes = document.evaluate( "//body//text()", document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null); 
+
+   for (var i = 0; i < textnodes.snapshotLength; i++) { 
+      node = textnodes.snapshotItem(i); 
+      s = node.data;
+     
+	 
+      s = s.replace(/trending/gi, "");
+	  
+      node.data = s;
+   } 
+
+})();
